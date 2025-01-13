fcb9b55740c77b5a422fced3fd9191e67c71e8e8
theblaze98 <dionijvargas@gmail.com>
2025-01-12 21:01:19 -0400

    feat: 🎇remove default controller and service, set global API prefix and enable CORS

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+
+  app.setGlobalPrefix('api')
+
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,PUT,PATCH,POST,DELETE',
+  })
+
   await app.listen(process.env.PORT ?? 3000)
 }
 bootstrap()
