22d118f875c413a8ffb9218f794e119432da7558
theblaze98 <dionijvargas@gmail.com>
2025-02-01 21:00:07 -0400

    fix: 🐛remove trailing semicolon from CommonModule import in app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common'
 import { DrizzleModule } from './drizzle/drizzle.module'
 import { UsersModule } from './users/users.module'
-import { CommonModule } from './common/common.module';
+import { CommonModule } from './common/common.module'
 
 @Module({
   imports: [DrizzleModule, UsersModule, CommonModule],
