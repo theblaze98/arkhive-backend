3b784795dd663a763267f1e6fc9bf64e5c3014f4
theblaze98 <dionijvargas@gmail.com>
2025-02-08 22:15:49 -0400

    feat: 🎇load environment variables conditionally in development; add transaction repository file

--- src/drizzle/helpers/connection.ts
+++ src/drizzle/helpers/connection.ts
@@ -1,6 +1,8 @@
 import { neon } from '@neondatabase/serverless'
 
-process.loadEnvFile('.env.local')
+if (process.env.NODE_ENV === 'development') {
+  process.loadEnvFile('.env.local')
+}
 
 export const connection = () => {
   return neon(process.env.NEON_DATABASE_URL)
